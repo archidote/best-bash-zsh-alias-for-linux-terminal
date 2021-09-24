9cd208743491ccb9ff47806b6cd274aa57b020ae
brlndtech <brlndtech@gmail.com>
2021-09-24 15:15:29 +0200

    dfg

--- run.sh
+++ run.sh
@@ -2,121 +2,121 @@
 aliases=$(cat <<EOF
 # start_bestAliasLinux
 # alias added by bestAliasLinux   
-alias 1='sudo apt update && apt upgrade'
+alias 1="sudo apt update && apt upgrade"
  
 # alias added by bestAliasLinux   
-alias 2='sudo apt autoremove && apt purge'
+alias 2="sudo apt autoremove && apt purge"
  
 # alias added by bestAliasLinux   
-alias p='ping'
+alias p="ping"
  
 # alias added by bestAliasLinux   
-alias c='clear'
+alias c="clear"
 
 # alias added by bestAliasLinux   
-alias d='date'
+alias d="date"
  
 # alias added by bestAliasLinux   
-alias e='exit'
+alias e="exit"
  
 # alias added by bestAliasLinux   
-alias uu='sudo apt update && apt upgrade'
+alias uu="sudo apt update && apt upgrade"
 
 # alias added by bestAliasLinux   
-alias jj='sudo apt autoremove'
+alias jj="sudo apt autoremove"
  
 # alias added by bestAliasLinux   
-alias ai='sudo apt install'
+alias ai="sudo apt install"
 
 # alias added by bestAliasLinux   
-alias nn='sudo apt purge'
+alias nn="sudo apt purge"
  
 # alias added by bestAliasLinux   
-alias l='sudo ls -alt'
+alias l="sudo ls -alt"
 
 # alias added by bestAliasLinux   
-alias cdh='cd $HOME'
+alias cdh="cd $HOME"
 
 # alias added by bestAliasLinux   
-alias f="sudo find -name \$1\"
+alias f="sudo find -name $1"
 
 # alias added by bestAliasLinux   
-alias fn='sudo find / -name $1'
+alias fn="sudo find / -name $1"
 
 # alias added by bestAliasLinux   
-alias ..='cd ..'
+alias ..="cd .."
 
 # alias added by bestAliasLinux   
-alias b='cd -'
+alias b="cd -"
  
 # alias added by bestAliasLinux   
-alias cdw='cd /var/www/html/'
+alias cdw="cd /var/www/html/"
 
 # alias added by bestAliasLinux   
-alias cdl='cd /var/log/'
+alias cdl="cd /var/log/"
 
 # alias added by bestAliasLinux   
-alias Gc='git clone $1'
+alias Gc="git clone $1"
 
 # alias added by bestAliasLinux   
-alias ga='git add .'
+alias ga="git add ."
 
 # alias added by bestAliasLinux   
-alias gs='git status'
+alias gs="git status"
 
 # alias added by bestAliasLinux   
-alias gpom='git push origin master'
+alias gpom="git push origin master"
 
 # alias added by bestAliasLinux   
-alias gpo='git push origin $1'
+alias gpo="git push origin $1"
 
 # alias added by bestAliasLinux   
-alias sr='sudo systemctl restart $1'
+alias sr="sudo systemctl restart $1"
 
 # alias added by bestAliasLinux   
-alias sss='sudo systemctl status $1'
+alias sss="sudo systemctl status $1"
 
 # alias added by bestAliasLinux   
-alias strt='sudo systemctl start $1'
+alias strt="sudo systemctl start $1"
 
 # alias added by bestAliasLinux   
-alias stp='sudo systemctl stop $1'
+alias stp="sudo systemctl stop $1"
 
 # alias added by bestAliasLinux   
-alias se='sudo systemctl enable $1'
+alias se="sudo systemctl enable $1"
 
 # alias added by bestAliasLinux   
-alias ch='sudo hostnamectl set-hostname $1'
+alias ch="sudo hostnamectl set-hostname $1"
 
 # alias added by bestAliasLinux  
-alias s='sudo su'
+alias s="sudo su"
 
 # alias added by bestAliasLinux  
-alias pi='ping 1.1.1.1'
+alias pi="ping 1.1.1.1"
 
 # alias added by bestAliasLinux  
-alias pii='ping t.co'
+alias pii="ping t.co"
 
 # alias added by bestAliasLinux  
-alias dlc='docker ps'
+alias dlc="docker ps"
 
 # alias added by bestAliasLinux  
-alias dlca='docker ps -a'
+alias dlca="docker ps -a"
 
 # alias added by bestAliasLinux  
-alias ds='docker start $1'
+alias ds="docker start $1"
 
 # alias added by bestAliasLinux  
-alias dss='docker stop $1'
+alias dss="docker stop $1"
 
 # alias added by bestAliasLinux  
-alias de='docker exec -it $1 bash'
+alias de="docker exec -it $1 bash"
 
 # alias added by bestAliasLinux  
-alias dr='docker run -d $1'
+alias dr="docker run -d $1"
 
 # alias added by bestAliasLinux  
-alias dli='docker images'
+alias dli="docker images"
 
 # end_bestAliasLinux
 EOF
