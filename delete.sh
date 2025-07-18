5478c79176f27487dbe8e74d97fba91574869c1f
archidote <archidote.contact@gmail.com>
2025-07-18 11:34:48 +0200

    new alias

--- delete.sh
+++ delete.sh
@@ -1,20 +1,25 @@
-#!/bin/bash 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, please choose one of them : bash or zsh"
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied, please choose one of them: bash or zsh"
+    exit 1
 fi
+
 if [ "$1" == "bash" ]; then
-	sed -i '/start_bestAliasLinux/,/end_bestAliasLinux/d' $HOME/.bashrc 
-	if [ $? -eq 0 ]; then 
-	    echo "Done! Open a new terminal or run: source ~/.zshrc"
-	else
-	    echo "Unexpected error"
-	fi
-elif [ "$1" == "zsh" ]; then 
-	sed -i '/start_bestAliasLinux/,/end_bestAliasLinux/d' $HOME/.zshrc 
-	if [ $? -eq 0 ]; then
-		echo "Done! Open a new terminal or run: source ~/.zshrc" 
-	else
-	    echo "Unexpected error"
-	fi
+    sed -i '/start_bestAliasLinux/,/end_bestAliasLinux/d' "$HOME/.bashrc"
+    if [ $? -eq 0 ]; then
+        echo "Done! Open a new terminal or run: source ~/.bashrc"
+    else
+        echo "Unexpected error"
+    fi
+elif [ "$1" == "zsh" ]; then
+    sed -i '/start_bestAliasLinux/,/end_bestAliasLinux/d' "$HOME/.zshrc"
+    if [ $? -eq 0 ]; then
+        echo "Done! Open a new terminal or run: source ~/.zshrc (in a zsh shell)"
+    else
+        echo "Unexpected error"
+    fi
+else
+    echo "Invalid argument: $1. Please use 'bash' or 'zsh'."
+    exit 1
 fi
